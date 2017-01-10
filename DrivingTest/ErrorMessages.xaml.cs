38e6d17d813ba5700c7701a7d116914437526f2e
ToLeave <hy888716@gmail.com>
2017-01-10 16:25:48 +0800

    201701101625

--- DrivingTest/ErrorMessages.xaml.cs
+++ DrivingTest/ErrorMessages.xaml.cs
@@ -118,7 +118,9 @@ namespace DrivingTest
             //textBlock6.Text = "正确答案为:" + PublicClass.question_answer;
             textBlock6.Text = "正确答案为:" + PublicClass.question_answer;
 
-            timer.Interval = new TimeSpan(10000000);   //时间间隔为一秒
+            timer.Interval = new TimeSpan(10000);   //时间间隔为一秒
+            //int time = 10;
+            //textBlock7.Text = time.ToString() ;
             timer.Tick += new EventHandler(timer_Tick);
             timer.Start();//启动计时器
         }
